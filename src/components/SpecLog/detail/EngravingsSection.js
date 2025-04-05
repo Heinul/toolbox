66a7ace0fd3a301fdd9c8d992f8abc5b78e672d5
Heinul <henull.play@gmail.com>
2025-04-06 01:15:49 +0900

    보석 신규 효과 추가

--- src/components/SpecLog/detail/EngravingsSection.js
+++ src/components/SpecLog/detail/EngravingsSection.js
@@ -9,7 +9,7 @@ const EngravingsSection = ({ newData, oldData, hasComparison }) => {
     <div className="data-engravings">
       <h4>각인 정보</h4>
       <div className="engravings-list">
-        {/* 각인 정보 처리 로직 */}
+        {/* 현재 각인 렌더링 */}
         {newData.display.engr.map((engr, index) => {
           const oldEngr = hasComparison && oldData.display && oldData.display.engr && 
                 oldData.display.engr.find(e => e[0] === engr[0]);
@@ -21,9 +21,17 @@ const EngravingsSection = ({ newData, oldData, hasComparison }) => {
           const totalNew = engr[1][0] + engr[1][1];
           const totalOld = oldEngr ? oldEngr[1][0] + oldEngr[1][1] : 0;
           
+          const isNewEngraving = hasComparison && !oldEngr;
+          
           return (
-            <div key={index} className={`engraving-item ${hasChanged ? 'item-changed' : ''}`}>
-              <div className="engraving-name">{engr[0]}</div>
+            <div 
+              key={index} 
+              className={`engraving-item ${isNewEngraving ? 'new-engraving' : ''} ${hasChanged ? 'item-changed' : ''}`}
+            >
+              <div className="engraving-name">
+                {engr[0]}
+                {isNewEngraving && <span className="new-tag">NEW</span>}
+              </div>
               <div className="engraving-level">
                 <div>등급: {engr[1][0]}</div>
                 <div>레벨: {engr[1][1]}</div>
@@ -40,22 +48,27 @@ const EngravingsSection = ({ newData, oldData, hasComparison }) => {
           );
         })}
 
-        {/* 삭제된 각인 표시 */}
+        {/* 삭제된 각인 렌더링 */}
         {hasComparison && oldData.display && oldData.display.engr &&
-          oldData.display.engr.filter(oldEngr => 
-            !newData.display.engr.some(engr => engr[0] === oldEngr[0])
-          ).map((removedEngr, index) => (
-            <div key={`removed-${index}`} className="engraving-item removed-item">
-              <div className="engraving-name">{removedEngr[0]}</div>
-              <div className="engraving-level">
-                <div>삭제됨</div>
-                <div>
-                  {removedEngr[1][0] > 0 ? `등급 ${removedEngr[1][0]}` : ''}
-                  {removedEngr[1][1] > 0 ? ` 레벨 ${removedEngr[1][1]}` : ''}
+          oldData.display.engr
+            .filter(oldEngr => 
+              !newData.display.engr.some(engr => engr[0] === oldEngr[0])
+            )
+            .map((removedEngr, index) => (
+              <div 
+                key={`removed-${index}`} 
+                className="engraving-item removed-engraving"
+              >
+                <div className="engraving-name">{removedEngr[0]}</div>
+                <div className="engraving-level">
+                  <div>삭제됨</div>
+                  <div>
+                    {removedEngr[1][0] > 0 ? `등급 ${removedEngr[1][0]}` : ''}
+                    {removedEngr[1][1] > 0 ? ` 레벨 ${removedEngr[1][1]}` : ''}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
+            ))
         }
       </div>
     </div>
