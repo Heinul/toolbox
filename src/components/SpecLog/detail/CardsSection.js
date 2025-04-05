c09ec0cb249830ddf73dc7371a43d84147aa066b
Heinul <henull.play@gmail.com>
2025-04-06 01:24:36 +0900

    카드 표기 정렬 수정

--- src/components/SpecLog/detail/CardsSection.js
+++ src/components/SpecLog/detail/CardsSection.js
@@ -9,12 +9,17 @@ const CardsSection = ({ newData, oldData, hasComparison }) => {
     <div className="data-cards">
       <h4>카드 정보</h4>
       <div className="cards-list">
-        {Object.entries(newData.display.card).map(([cardName, level], index) => (
-          <div key={index} className="card-item">
-            <div className="card-name">{cardName}</div>
-            <div className="card-level">{level}각</div>
-          </div>
-        ))}
+        {Object.entries(newData.display.card).map(([cardName, awakeningCount], index) => {
+          // 현재 코드에서 변경 감지 로직 제거
+          return (
+            <div key={index} className="card-item">
+              <div className="card-content">
+                <div className="card-name">{cardName}</div>
+                <div className="card-level">{awakeningCount}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
